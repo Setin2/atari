cfb80e5ca6d36998216a266ab40e9f4c9960f893
Remus Stefan Cernat <68604849+Setin2@users.noreply.github.com>
2022-11-17 15:34:42 +0100

    Update atari.py

--- atari.py
+++ atari.py
@@ -134,7 +134,7 @@ class SARSA():
     return total_reward, loss
   
   def reshape_image(self, image):
-    if len(image[0]) is 210:
+    if len(image[0]) == 210:
         image = image[0]
     image_data = cv2.cvtColor(cv2.resize(image, (84, 84)), cv2.COLOR_BGR2GRAY)
     image_data[image_data > 0] = 255
